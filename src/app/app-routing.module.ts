0a99518bb093dd57c369ad791a3672a65f6add13
xpl0t <sheepr4ider@gmail.com>
2021-04-10 17:18:19 +0200

    Added default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/write-message', pathMatch: 'full' },
   { path: 'write-message', loadChildren: () => import('./write-message/write-message.module').then(m => m.WriteMessageModule) },
   { path: 'read-message', loadChildren: () => import('./read-message/read-message.module').then(m => m.ReadMessageModule) }
 ];
