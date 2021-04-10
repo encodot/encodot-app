a2b5b53757d15c915f492f67511b37c4d12c8e2f
xpl0t <sheepr4ider@gmail.com>
2021-04-10 21:16:33 +0200

    Refactored write-message component

--- src/app/write-message/write-message.component.ts
+++ src/app/write-message/write-message.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { of, Subscription } from 'rxjs';
-import { concatMap, tap } from 'rxjs/operators';
-import { WriteMessageRequestService } from './write-message-request.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import * as forge from 'node-forge';
+import { Subscription } from 'rxjs';
+import { concatMap, tap } from 'rxjs/operators';
+import { MessageMetadata } from './models/message-metadata.model';
+import { WriteMessageRequestService } from './write-message-request.service';
 
 @Component({
   selector: 'app-write-message',
@@ -19,8 +21,19 @@ export class WriteMessageComponent implements OnInit, OnDestroy {
     password: new FormControl(null, [ Validators.required ])
   });
 
+  public messageMetadata: MessageMetadata = {
+    id: 'dfkajlfkjkejljewlkfjkejjefkej',
+    urlPassword: 'jdjdjdjdjdjdjjdjjdjdjddjdj'
+  };
+
+  public get url(): string {
+    const { id, urlPassword } = this.messageMetadata;
+    return `${ window.location.host }/read-message?messageId=${ id }&urlPassword=${ urlPassword }`;
+  }
+
   public constructor(
-    private reqSv: WriteMessageRequestService
+    private reqSv: WriteMessageRequestService,
+    private snackbar: MatSnackBar
   ) { }
 
   public ngOnInit(): void {
@@ -52,9 +65,16 @@ export class WriteMessageComponent implements OnInit, OnDestroy {
       })
     ).subscribe(m => {
       console.log(m);
+      this.messageMetadata = m;
     }, e => {
       console.error('Could not send message :(', e);
     })
+  }
+
+  public successfullyCopiedToClipboard(): void {
+    this.snackbar.open('Copied message URL to clipboard! :)', null, {
+      duration: 2000
+    });
   }
 
 }
--- src/app/write-message/write-message.module.ts
+++ src/app/write-message/write-message.module.ts
@@ -1,10 +1,13 @@
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { WriteMessageRequestService } from './write-message-request.service';
 import { WriteMessageRoutingModule } from './write-message-routing.module';
 import { WriteMessageComponent } from './write-message.component';
@@ -16,10 +19,13 @@ import { WriteMessageComponent } from './write-message.component';
   imports: [
     CommonModule,
     WriteMessageRoutingModule,
+    ClipboardModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
+    MatSnackBarModule,
     MatButtonModule,
+    MatCardModule,
     FormsModule,
     ReactiveFormsModule
   ],
