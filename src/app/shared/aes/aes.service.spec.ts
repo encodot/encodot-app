3c784d49812a39ef975e087c2f77d33f30a99f8b
xpl0t <sheepr4ider@gmail.com>
2021-10-17 21:15:08 +0200

    Added AesService tests

--- src/app/shared/aes/aes.service.spec.ts
+++ src/app/shared/aes/aes.service.spec.ts
@@ -29,16 +29,38 @@ describe('AesService', () => {
   });
 
   test('utf-8 special characters should be handled correctly', () => {
-    const randomString = (length: number, from: number, count: number): string => Array.from(
-      { length }, () => String.fromCharCode(from + Math.floor(Math.random() * count))
-    ).join('');
-
-    const str = '😈😋😇😈😓𝌃𝌍𝌒𝌆𝌉'; // randomUnicodeString(500);
-    const password = 'test'; // randomUnicodeString(500);
+    const str = '😈😋😇😈😓𝌃𝌍𝌒𝌆𝌉[]{}^^°°<>!"§$%&/()=?`´*+~#’µ·….,–|@€';
+    const password = '[]{}^^°°<>!"§$%&/()=?`´*+~#’µ·….,–|@€';
     const cipher = service.encrypt(str, password);
     const decrypted = service.decrypt(cipher, password);
 
     expect(decrypted).toBe(str);
+  });
+
+  test('object should be the same after it was encrypted and then decrypted', () => {
+    const obj1 = {
+      t0: '😈😋😇😈😓𝌃𝌍𝌒𝌆𝌉',
+      t1: Number.MIN_SAFE_INTEGER,
+      t2: true,
+      t3: false,
+      t4: null,
+      t5: undefined,
+      dontEncrypt: 'Tests are marvelous'
+    };
+
+    const password = '[]{}^^°°<>!"§$%&/()=?`´*+~#’µ·….,–|@€';
+    const cipherObj = service.encryptObj(obj1, password, [ 't0', 't1', 't2', 't3', 't4', 't5' ]);
+    const obj2 = service.decryptObj(cipherObj, password, [ 't0', 't1', 't2', 't3', 't4', 't5' ]);
+
+    expect(obj2).toMatchObject(obj1);
+  });
+
+  test('cipher should be URL safe', () => {
+    const str = 'Test';
+    const password = 'area51';
+    const cipher = service.encrypt(str, password);
+
+    expect(cipher).toMatch(/^([a-zA-Z0-9\.\_\-\~])*$/);
   });
 
 });
