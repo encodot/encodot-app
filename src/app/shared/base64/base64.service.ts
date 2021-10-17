5a2b87e584f900cca0464d2988c5d8955dbf199e
xpl0t <sheepr4ider@gmail.com>
2021-10-17 21:08:55 +0200

    Fixed base64 service

--- src/app/shared/base64/base64.service.ts
+++ src/app/shared/base64/base64.service.ts
@@ -14,11 +14,11 @@ export class Base64Service {
 
   public encodeUrl(bytes: string): string {
     const base64 = this.encode(bytes);
-    return base64.replace('+', '-').replace('/', '_').replace(/=+$/, '');
+    return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '');
   }
 
   public decodeUrl(urlBase64: string): string {
-    let base64 = urlBase64.replace('-', '+').replace('_', '/');
+    let base64 = urlBase64.replace(/\-/g, '+').replace(/\_/g, '/');
 
     while (base64.length % 4) {
       base64 += '=';
