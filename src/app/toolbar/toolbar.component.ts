df30354c5161a751a21c472c785ada87de9e1f09
xpl0t <sheepr4ider@gmail.com>
2021-10-31 20:55:25 +0100

    Added start page button

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -3,7 +3,8 @@ import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-toolbar',
-  templateUrl: './toolbar.component.html'
+  templateUrl: './toolbar.component.html',
+  styleUrls: [ './toolbar.component.scss' ]
 })
 export class ToolbarComponent {
 
